cf7df327f045e5d41431213571ec178222e1795a
Kolyago Artyom <stakkato95@gmail.com>
2015-05-02 13:07:32 +0300

    added completely correct translation

---
+++ CourseProject/CourseProject/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CourseProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopMenu = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.uploadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextBoxLatin = new System.Windows.Forms.TextBox();
+            this.TextBoxTranslated = new System.Windows.Forms.TextBox();
+            this.ButtonDoTranslation = new System.Windows.Forms.Button();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TopMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopMenu
+            // 
+            this.TopMenu.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.TopMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.TopMenu.Location = new System.Drawing.Point(0, 0);
+            this.TopMenu.Name = "TopMenu";
+            this.TopMenu.Size = new System.Drawing.Size(584, 24);
+            this.TopMenu.TabIndex = 0;
+            // 
+            // MenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uploadFileToolStripMenuItem,
+            this.aboutProjectToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "MenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // uploadFileToolStripMenuItem
+            // 
+            this.uploadFileToolStripMenuItem.Name = "uploadFileToolStripMenuItem";
+            this.uploadFileToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.uploadFileToolStripMenuItem.Text = "Upload File";
+            // 
+            // aboutProjectToolStripMenuItem
+            // 
+            this.aboutProjectToolStripMenuItem.Name = "aboutProjectToolStripMenuItem";
+            this.aboutProjectToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.aboutProjectToolStripMenuItem.Text = "About project";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // TextBoxLatin
+            // 
+            this.TextBoxLatin.Location = new System.Drawing.Point(12, 27);
+            this.TextBoxLatin.Multiline = true;
+            this.TextBoxLatin.Name = "TextBoxLatin";
+            this.TextBoxLatin.Size = new System.Drawing.Size(250, 180);
+            this.TextBoxLatin.TabIndex = 1;
+            // 
+            // TextBoxTranslated
+            // 
+            this.TextBoxTranslated.Location = new System.Drawing.Point(322, 27);
+            this.TextBoxTranslated.Multiline = true;
+            this.TextBoxTranslated.Name = "TextBoxTranslated";
+            this.TextBoxTranslated.Size = new System.Drawing.Size(250, 180);
+            this.TextBoxTranslated.TabIndex = 2;
+            // 
+            // ButtonDoTranslation
+            // 
+            this.ButtonDoTranslation.BackgroundImage = global::CourseProject.Properties.Resources.arrow;
+            this.ButtonDoTranslation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonDoTranslation.Location = new System.Drawing.Point(275, 27);
+            this.ButtonDoTranslation.Name = "ButtonDoTranslation";
+            this.ButtonDoTranslation.Size = new System.Drawing.Size(35, 35);
+            this.ButtonDoTranslation.TabIndex = 3;
+            this.ButtonDoTranslation.UseVisualStyleBackColor = true;
+            this.ButtonDoTranslation.Click += new System.EventHandler(this.ButtonDoTranslation_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(32, 19);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += settings_Click;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.ButtonDoTranslation);
+            this.Controls.Add(this.TextBoxTranslated);
+            this.Controls.Add(this.TextBoxLatin);
+            this.Controls.Add(this.TopMenu);
+            this.MainMenuStrip = this.TopMenu;
+            this.MaximumSize = new System.Drawing.Size(600, 300);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "MainForm";
+            this.Text = "Translator";
+            this.TopMenu.ResumeLayout(false);
+            this.TopMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip TopMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem uploadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox TextBoxLatin;
+        private System.Windows.Forms.TextBox TextBoxTranslated;
+        private System.Windows.Forms.Button ButtonDoTranslation;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    }
+}
+
---
+++ CourseProject/CourseProject/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseProject
+{
+    public partial class MainForm : Form
+    {
+        Dictionary<string, char> TranslationDictionary = new Dictionary<string, char>()
+        {
+            {"a", 'а'},
+            {"b", 'б'},
+            {"c", 'ц'},
+            {"d", 'д'},
+            {"e", 'е'},
+            {"f", 'ф'},
+            {"g", 'г'},
+            {"h", 'х'},
+            {"i", 'и'},
+            {"j", 'й'},
+            {"k", 'к'},
+            {"l", 'л'},
+            {"m", 'м'},
+            {"n", 'н'},
+            {"o", 'о'},
+            {"p", 'п'},
+            {"r", 'р'},
+            {"s", 'с'},
+            {"t", 'т'},
+            {"u", 'у'},
+            {"v", 'в'},
+            {"x", 'х'},
+            {"y", 'ы'},
+            {"z", 'з'},
+            {"zh", 'ж'},
+            {"ch", 'ч'},
+            {"sh", 'ш'},
+            {"shch", 'щ'},
+            {"shh", 'щ'},
+            {"yu", 'ю'},
+            {"ju", 'ю'},
+            {"io", 'ё'},
+            {"jo", 'ё'},
+            {"yo", 'ё'},
+            {"je", 'э'},
+            {"ts", 'ц'},
+            {"tc", 'ц'},
+            {"ja", 'я'},
+            {"jj", 'й'},
+            {"kh", 'х'},
+            {"'", 'ь'},
+        };
+
+        List<char> TwoSymbolsTranslation = new List<char>()
+        {
+            'z',
+            'c',
+            's',
+            'y',
+            't',
+            'i',
+            't',
+            'j',
+            'k',
+        };
+
+        List<char> ThreeSymbolsTranslation = new List<char>()
+        {
+            's',
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonDoTranslation_Click(object sender, EventArgs e)
+        {
+            string latinText = TextBoxLatin.Text;
+            string translatedText = null;
+
+            for (int i = 0; i < latinText.Length; i++)
+            {
+                string character = latinText[i].ToString().ToLower();
+                if (TranslationDictionary.ContainsKey(character))
+                {
+                    //explain on examples: artjom
+                    if (ThreeSymbolsTranslation.Contains(char.Parse(character)) && i + 2 <= latinText.Length
+                        && !String.IsNullOrWhiteSpace(latinText[i + 1].ToString())
+                        && !String.IsNullOrWhiteSpace(latinText[i + 2].ToString()))
+                    {
+                        //explain on example chasha
+                        //explain p'janyj (out of bounds array)
+                        character += latinText[i + 1].ToString() + latinText[i + 2].ToString();
+                        i += 2;
+                    }
+                    else if (TwoSymbolsTranslation.Contains(char.Parse(character)) && i + 1 <= latinText.Length
+                        && !String.IsNullOrWhiteSpace(latinText[i + 1].ToString()))
+                    {
+                        i += 1;
+                        character += latinText[i].ToString();
+                    }
+
+                    if (TranslationDictionary.ContainsKey(character))
+                    {
+                        translatedText += TranslationDictionary[character];
+                    }
+                    else
+                    {
+                        if (TwoSymbolsTranslation.Contains(character[0]))
+                        {
+                            //on example chasha
+                            character = character.Substring(0, character.Length - 1);
+                            i -= 1;
+                            if (TranslationDictionary.ContainsKey(character))
+                            {
+                                translatedText += TranslationDictionary[character];
+                            }
+                            else
+                            {
+                                translatedText += TranslationDictionary[character[0].ToString()];
+                                i -= 1;
+                            }
+                        }
+                        else
+                        {
+                            character = character.Substring(0, character.Length - 2);
+                            translatedText += TranslationDictionary[character];
+                            i -= 2;
+                        }
+                    }
+                }
+                else
+                {
+                    translatedText += character;
+                }
+            }
+
+            TextBoxTranslated.Text = translatedText;
+        }
+
+        private void settings_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
